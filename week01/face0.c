c6536cbde879b9c765e111437bfcfab162e7521f
Claire <claire.hua@unsw.edu.au>
2021-02-18 14:46:13 +1100

    week01 content uploaded

---
+++ week01/face0.c
@@ -0,0 +1,19 @@
+// face0
+// face0.c
+//
+// This program was written by COMP1511 H13C
+// on 18/02/2021
+//
+// Prints a face
+
+
+#include <stdio.h>
+
+int main (void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+
+    return 0;
+}
---
+++ week01/face1.c
@@ -0,0 +1,23 @@
+// face1
+// face1.c
+//
+// This program was written by COMP1511 H13C
+// on 18/02/2021
+//
+// Prints a face (with a smile)
+
+
+#include <stdio.h>
+
+int main (void) {
+    
+    // this is better
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf("\\_/\n");
+    
+    // please don't do this :)
+    printf("~ ~\n0 0\n o\n\\_/\n");
+    return 0;
+}
