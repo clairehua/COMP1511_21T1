ca15fac7c3ae1327875632e9249e1e40ef5c70f4
Claire <claire.hua@unsw.edu.au>
2021-02-25 14:34:36 +1100

    week2 content added

---
+++ week02/rectangle.c
@@ -0,0 +1,20 @@
+// COMP1511 H13C
+
+
+#include <stdio.h>
+
+int main(void){
+
+    int width;
+    int length;
+    
+    scanf("%d %d", &width, &length);
+
+    int area = width * length;
+    
+    printf("The area of the rectangle is: %d\n", area);
+
+
+
+    return 0;
+} 
